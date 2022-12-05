2ae348aaa359e79a2946c16d857cfab820dd4c70
Michael Meckler <rattroupe@reiterate-app.com>
2022-12-05 10:28:54 -0800

    Reorganized directory structure

---
+++ lib/mastodon-social/mastodon-social.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "jekyll"
+require_relative "version"
+
+module MastodonSocial
+  class Error < StandardError; end
+end
+
+def require_all(group)
+  Dir[File.expand_path("#{group}/*.rb", __dir__)].each do |file|
+    require file
+  end
+end
+
+require_all "commands"
+require_all "generators"
---
+++ lib/mastodon-social/version.rb
@@ -0,0 +1,3 @@
+module MastodonSocial
+  VERSION = "0.1.0"
+end
